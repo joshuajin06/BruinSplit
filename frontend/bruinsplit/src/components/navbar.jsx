31a27584e29229a2345d45df10f5c7ca3d8705c6
justinluc <justinlucapp@gmail.com>
2025-10-30 18:05:12 -0700

    Small changes to frontends react: changed background color and made the logo a button

--- frontend/bruinsplit/src/components/navbar.jsx
+++ frontend/bruinsplit/src/components/navbar.jsx
@@ -5,9 +5,8 @@ import "./navbar.css"; // Import the CSS file
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <h2>BruinSplit</h2>
+      <h2 className="siteTitle"><Link to="/">BruinSplit</Link></h2>
       <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
       </ul>
