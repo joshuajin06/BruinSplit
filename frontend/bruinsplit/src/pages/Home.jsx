41f74e3ae7b3185d00989f4b27b19888182b6990
justinluc <justinlucapp@gmail.com>
2025-11-02 11:58:13 -0800

    Added a footer

--- frontend/bruinsplit/src/pages/Home.jsx
+++ frontend/bruinsplit/src/pages/Home.jsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <h1>Hello!</h1>;
+import React from 'react';
+import './pages.css';
+
+export default function Home() {  
+  return (
+    <div className="page-container">
+      <div className="content-wrap">
+        <h1>Welcome to BruinSplit!</h1>
+        <h1>Welcome to BruinSplit!</h1>
+        <h1>Welcome to BruinSplit!</h1>
+        <h1>Welcome to BruinSplit!</h1>
+        <h1>Welcome to BruinSplit!</h1>
+        <h1>Welcome to BruinSplit!</h1>
+        <h1>Welcome to BruinSplit!</h1>
+        <h1>Welcome to BruinSplit!</h1>
+        <h1>Welcome to BruinSplit!</h1>
+        <h1>Welcome to BruinSplit!</h1>
+        <h1>Welcome to BruinSplit!</h1>
+        <h1>Welcome to BruinSplit!</h1>
+      
+      </div>
+    </div>
+  );
 }
