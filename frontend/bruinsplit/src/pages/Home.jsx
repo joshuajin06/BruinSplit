8206d99f521be03154d2fcd6048e47d40c2eed7e
justinluc <justinlucapp@gmail.com>
2025-11-02 17:59:59 -0800

    Added rudementary card components that hold text (skeleton for the ride share posts) -can't seem to get them to line up side by side

--- frontend/bruinsplit/src/pages/Home.jsx
+++ frontend/bruinsplit/src/pages/Home.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import './pages.css';
+import Card from '../components/card.jsx';
 
 export default function Home() {  
   return (
     <>
-        <h1>Welcome to BruinSplit!</h1>
-        <h1>Welcome to BruinSplit!</h1>
-        <h1>Welcome to BruinSplit!</h1>
-        <h1>Welcome to BruinSplit!</h1>
-        <h1>Welcome to BruinSplit!</h1>
-        <h1>Welcome to BruinSplit!</h1>
-        <h1>Welcome to BruinSplit!</h1>
-        <h1>Welcome to BruinSplit!</h1>
-        <h1>Welcome to BruinSplit!</h1>
-        <h1>Welcome to BruinSplit!</h1>
-        <h1>Welcome to BruinSplit!</h1>
-        
+        <div className="page-container">
+          <div className='content-wrap'>
+          <h1>Welcome to BruinSplit!</h1>
+            <Card title="Ride Dest 1 -> Dest 2" content="Yeah this is a ride"/>
+            <Card title="Ride Dest 3 -> Dest 4" content="This is another ride"/>
+          </div>
+        </div>
+
     </>
   );
-}
+} 
