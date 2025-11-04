d0e762c6582695aaa8bac16f43bccfe13b64fa62
justinluc <justinlucapp@gmail.com>
2025-11-04 14:59:40 -0800

    changes to navbar

--- frontend/bruinsplit/src/components/navbar.jsx
+++ frontend/bruinsplit/src/components/navbar.jsx
@@ -7,8 +7,11 @@ export default function Navbar() {
     <nav className="navbar">
       <h2 className="siteTitle"><Link to="/">BruinSplit</Link></h2> 
       <ul className="nav-links">
+        <li><Link to="/postings">Postings</Link></li>
+        <li><Link to="/events">Events</Link></li>
+        <li><Link to="/my-rides">My Rides</Link></li>
         <li><Link to="/login">Login</Link></li>
       </ul>
     </nav>
   );
-} 
+}   
