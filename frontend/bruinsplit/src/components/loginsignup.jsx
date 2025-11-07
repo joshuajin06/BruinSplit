c022f00a9add42705493c5be3e85af52444827b6
justinluc <justinlucapp@gmail.com>
2025-11-07 10:44:54 -0800

    Fixed the sign up login screen and added toggle

--- frontend/bruinsplit/src/components/loginsignup.jsx
+++ frontend/bruinsplit/src/components/loginsignup.jsx
@@ -3,26 +3,37 @@ import Person from '../assets/person.png';
 import './loginsignup.css';
 
 export default function LoginSignup() {
-    const [signUpOrLogin, setSignUpOrLogin] = useState('Sign Up');
+    const [loggedIn, setLoggedIn] = useState(true);
 
     return (
         <div className="body">
             <div className="login-page-container"> 
                 <div className='header'>
-                    <div className='text'><h1>{ signUpOrLogin }</h1></div>
+                    <div className='text'><h1>{loggedIn ? "Login" : "Sign Up"}</h1></div>
                     <div className='underline'></div>
                 </div>
                 <div className='input'>
                     <input type="text" placeholder="Username" className='username-input'/>
                 </div>
+                {loggedIn? null :
+                        <div className='input'>
+                            <input type="text" placeholder="Name" className='name-input'/>
+                        </div>
+                    }
+                <div>
                 <div className='input'>
                     <input type="text" placeholder="Email" className='email-input'/>
                 </div>
                 <div className='input'>
                     <input type="password" placeholder="Password" className='password-input'/>
                 </div>
-                <div>
-                    <button className='login-signup-button'>{ signUpOrLogin }</button>
+                <div className='toggle-action'>
+                    <span className="toggle-action-button" onClick={() => { setLoggedIn(!loggedIn) }}>
+                        {loggedIn ? "Don't have an account? " : "Already have an account? "}
+                        {loggedIn ? false : true}
+                    </span>
+                </div>
+                    <button className='login-signup-button'>{loggedIn ? "Login" : "Sign Up"}</button>
                 </div>
             </div>
         </div>
