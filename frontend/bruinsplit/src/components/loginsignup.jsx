abeeea9ed51a73373236216e3dd14ccb5bf8da0d
justinluc <justinlucapp@gmail.com>
2025-11-06 17:15:54 -0800

    Dowloaded material UI modules for future use

--- frontend/bruinsplit/src/components/loginsignup.jsx
+++ frontend/bruinsplit/src/components/loginsignup.jsx
@@ -1,28 +1,30 @@
 import { useState } from 'react'
+import Person from '../assets/person.png';
 import './loginsignup.css';
 
 export default function LoginSignup() {
     const [signUpOrLogin, setSignUpOrLogin] = useState('Sign Up');
 
     return (
-        <>
-            <div className="page-container"> 
+        <div className="body">
+            <div className="login-page-container"> 
                 <div className='header'>
                     <div className='text'><h1>{ signUpOrLogin }</h1></div>
+                    <div className='underline'></div>
                 </div>
-                <div className='username-input'>
-                    <input type="text" placeholder="Username" />
+                <div className='input'>
+                    <input type="text" placeholder="Username" className='username-input'/>
                 </div>
-                <div className='email-input'>
-                    <input type="text" placeholder="Email" />
+                <div className='input'>
+                    <input type="text" placeholder="Email" className='email-input'/>
                 </div>
-                <div className='password-input'>
-                    <input type="password" placeholder="Password" />
+                <div className='input'>
+                    <input type="password" placeholder="Password" className='password-input'/>
                 </div>
                 <div>
                     <button className='login-signup-button'>{ signUpOrLogin }</button>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
--- frontend/bruinsplit/src/pages/Postings.jsx
+++ frontend/bruinsplit/src/pages/Postings.jsx
@@ -12,7 +12,6 @@ export default function Postings() {
                 <Card title="Ride Dest 3 to Dest 4" content="This is another ride" image="https://cdn.i-scmp.com/sites/default/files/styles/1200x800/public/images/methode/2017/05/19/2b2d8790-3c6a-11e7-8ee3-761f02c18070_1280x720_204107.jpg?itok=oBUq3Omm"/>
                 <Card title="Ride Dest 5 to Dest 6" content="Yet another ride"/>
 
-                
           </div>
         </div>;
     </>
