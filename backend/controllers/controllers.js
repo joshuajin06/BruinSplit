878a0848f2a7155ff37f9ffff851eb9dbc1abbae
jadenho10 <jaden.ho@gmail.com>
2025-11-06 16:42:55 -0800

    adding controllers

--- backend/controllers/controllers.js
+++ backend/controllers/controllers.js
@@ -1,1 +1,20 @@
-import { supabase } from '../src/lib/supabase.js';
+const supabase = require('../src/lib/supabase');
+
+const createUser = async (req, res) => {
+    try {
+        const {email, password, first_name, last_name, user_name, age} = req.body;
+        // check if all fields are provided
+        if (!email || !password || !first_name || !last_name || !user_name || !age) {
+            return res.status(400).json({ error: 'Missing required fields' });
+        }
+        // check if username already exists
+        const {data : existingUser} = await supabase
+            .from('users')
+            .select('user_name')
+            .eq('user_name', user_name)
+            .single();
+        
+    } catch (error) {
+        console.error('')
+    }
+};
