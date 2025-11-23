6d7a393d609b10a534e8fb435be347302ea8198e
justinluc <justinlucapp@gmail.com>
2025-11-22 22:52:16 -0800

    Event endpoints added: DELETE &  UPDATE

--- backend/src/routes/eventsRoute.js
+++ backend/src/routes/eventsRoute.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get("/", eventsController.getEvents);
 router.post("/", eventsController.createEvent);
+router.get(("/:id"), eventController.getEventById);
 router.put("/:id", eventsController.updateEvent);
 router.delete("/:id", eventsController.deleteEvent);
 
