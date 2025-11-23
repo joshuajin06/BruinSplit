8f084e7bc643ab03fcd65b04661d969f516433b6
justinluc <justinlucapp@gmail.com>
2025-11-22 22:34:01 -0800

    GET by ID added

--- backend/src/routes/eventsRoute.js
+++ backend/src/routes/eventsRoute.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.get("/", eventsController.getEvents);
 router.post("/", eventsController.createEvent);
+router.put("/:id", eventsController.updateEvent);
+router.delete("/:id", eventsController.deleteEvent);
 
 export default router;
