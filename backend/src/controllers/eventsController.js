cf788081e332cc33d8b84a1b7f99e769a9a6d12c
justinluc <justinlucapp@gmail.com>
2025-11-22 22:52:16 -0800

    Event endpoints added: DELETE &  UPDATE

--- backend/src/controllers/eventsController.js
+++ backend/src/controllers/eventsController.js
@@ -48,14 +48,40 @@ export const createEvent = async (req, res) => {
   }
 };
 
+//useful for event details
 export const getEventById = async (req, res) => {
-  const { id } = 1;//req.params; Place holder for now
+  const { id } = req.params; 
   const { data, error } = await supabase
     .from("events")
     .select("*")
     .eq("id", id)
-    .single();
+    .single(); //returns a single element
 
   if (error) return res.status(404).json({ error: "Event not found" });
   res.json(data);
 };
+
+export const deleteEvent = async (req, res) => {
+  const { id } = req.params;
+  const { data, error } = await supabase
+  .from("events")
+  .delete()
+  .eq("id", id);
+
+  if (error) return req.status(400).json({ error })
+  res.json({ message: "Event Deleted" });
+}
+
+export const updateEvent = async (req, res) => {
+  const { id } = reqparams;
+  const updates = req.body;
+
+  const { data, error } = await supabase
+  .from("events")
+  .update(updates)
+  .eq("id", id)
+  .select(); //selects all text from an html input field
+
+  if (error) return res.status(400).json({ error });
+  res.json(data[0]);
+}
