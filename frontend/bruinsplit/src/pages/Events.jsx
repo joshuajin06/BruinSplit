8177d66acaf4cc0b6de7f24b0028bda3f3ca1b3c
justinluc <justinlucapp@gmail.com>
2025-11-23 16:15:44 -0800

    Added a submenu for Card component

--- frontend/bruinsplit/src/pages/Events.jsx
+++ frontend/bruinsplit/src/pages/Events.jsx
@@ -1,11 +1,9 @@
 import './pages.css';
-import EventDisplay from "../components/eventsDisplay.jsx"
 
 export default function Events() {
     return(
         <>
             <h1>Events</h1>;
-            <EventDisplay />
         </>
     );
 
