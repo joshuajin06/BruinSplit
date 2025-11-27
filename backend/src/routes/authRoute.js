a33a645e4418b17a40922c95bdfdb28a1b6a48cb
Anish Kumar <anishkkrishkumar@gmail.com>
2025-11-27 13:56:00 -0800

    added scaffolding for testing, started service test for POST /api/ride - create valid ride

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -3,8 +3,8 @@ import { signup, login } from '../controllers/authController.js';
 
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/login', login);
+router.post('/signup', signup); // POST /api/auth/signup -> calls signup
+router.post('/login', login); // POST /api/auth/login -> calls login
 
 export default router;
 
