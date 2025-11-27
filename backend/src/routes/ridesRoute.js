97d4d06c61d3cde8a4246b5e1ae4639d6400c0ca
Anish Kumar <anishkkrishkumar@gmail.com>
2025-11-26 19:24:52 -0800

    created POST api/rides - create a ride

--- backend/src/routes/ridesRoute.js
+++ backend/src/routes/ridesRoute.js
@@ -1,13 +1,26 @@
 import express from 'express';
-import { getRides, getRideById } from '../controllers/ridesController.js';
+import { getRides, getRideById, postRide } from '../controllers/ridesController.js';
 import { authenticateUser } from '../middleware/authenticateUser.js';
 
 const router = express.Router();
+
+// POST /api/rides - allows user to create a rides post
+router.post('/', authenticateUser, postRide); // in progress
+
+// POST /api/rides/:id/join - Add a user to the ride
+// TODO
 
 // GET /api/rides - Get all rides (public, no auth needed)
 router.get('/', getRides);
 
 // GET /api/rides/:id - Get specific ride by ID (public)
 router.get('/:id', getRideById);
+
+
+// DELETE /api/rides/:id/leave - Leave a ride 
+// TODO
+
+// GET /api/rides/rides/my-rides - Get all rides a user has joined
+// TODO
 
 export default router;
