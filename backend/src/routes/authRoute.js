af786ea61ec5881d72e65c5bb581acd572f172c7
joshuajin06 <4928anzhe@gmail.com>
2025-11-29 12:12:45 -0800

    Refactoring auth controllers

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { supabase } from '../supabase.js';
-import { signup, login } from '../controllers/authController.js';
+import { signup, login, retrieveUser, logout, changePassword } from '../controllers/authController.js';
 import { authenticateUser } from '../middleware/authenticateUser.js';
 import { comparePassword, hashPassword } from '../utils/auth.js';
 
@@ -8,67 +8,9 @@ const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
-
-router.get('/me', authenticateUser, async (req, res) => {
-  try {
-    res.json(req.user);
-  } catch(error) {
-    res.status(400).json({error: error.message});
-  }
-})
-
-
-router.post('/logout', authenticateUser, async (req, res) => {
-  res.json({message: 'Logged out successfully'});
-})
-
-
-router.post('/change-password', authenticateUser, async (req, res) => {
-  try {
-    const {currentPassword, newPassword} = req.body;
-
-    if(!currentPassword || !newPassword) {
-      return res.status(400).json({error: "Current and new password are both required"});
-    }
-
-    if(newPassword.length < 8) {
-      res.status(400).json({error: "Password length must be 8 or longer"});
-    }
-
-    if(currentPassword == newPassword) {
-      return res.status(400).json({error: "New Password cannot be the same as the old password"});
-    }
-
-    const {data: user} = await supabase
-      .from('profiles')
-      .select('password_hash')
-      .eq('id', req.user.id)
-      .single()
-
-    const validPassword = await comparePassword(currentPassword, user.password_hash);
-
-    if(!validPassword) {
-      return res.status(400).json({error: "Current password is not valid"});
-    }
-
-    const newPasswordHash = await hashPassword(newPassword);
-
-    const { error } = await supabase
-      .from('profiles')
-      .update({password_hash : newPasswordHash})
-      .eq('id', req.user.id)
-
-    if(error) throw error;
-
-    res.json({ message: 'Password changed successfully' });
-
-  } catch (error) {
-
-    console.error(error);
-    res.status(400).json({ error: error.message || 'Failed to change password' });
-    
-  }
-})
+router.get('/me', authenticateUser, retrieveUser)
+router.post('/logout', authenticateUser, logout)
+router.post('/change-password', authenticateUser, changePassword)
 
 export default router;
 
