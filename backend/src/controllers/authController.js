684e76297ed44f3a082bb5187c2693eb478da19c
joshuajin06 <4928anzhe@gmail.com>
2025-11-29 12:12:45 -0800

    Refactoring auth controllers

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -60,3 +60,65 @@ export async function signup(req, res, next) {
     next(error);
   }
 }
+
+
+export async function retrieveUser(req, res, next) {
+  try {
+    res.json(req.user);
+  } catch(error) {
+    res.status(400).json({error: error.message});
+  }
+}
+
+
+export async function logout(req, res, next) {
+  res.json({message: 'Logged out successfully'});
+}
+
+
+export async function changePassword(req, res, next) {
+  try {
+      const {currentPassword, newPassword} = req.body;
+  
+      if(!currentPassword || !newPassword) {
+        return res.status(400).json({error: "Current and new password are both required"});
+      }
+  
+      if(newPassword.length < 8) {
+        return res.status(400).json({error: "Password length must be longer than 8"});
+      }
+  
+      if(currentPassword == newPassword) {
+        return res.status(400).json({error: "New Password cannot be the same as the old password"});
+      }
+  
+      const {data: user} = await supabase
+        .from('profiles')
+        .select('password_hash')
+        .eq('id', req.user.id)
+        .single()
+  
+      const validPassword = await comparePassword(currentPassword, user.password_hash);
+  
+      if(!validPassword) {
+        return res.status(400).json({error: "Current password is not valid"});
+      }
+  
+      const newPasswordHash = await hashPassword(newPassword);
+  
+      const { error } = await supabase
+        .from('profiles')
+        .update({password_hash : newPasswordHash})
+        .eq('id', req.user.id)
+  
+      if(error) throw error;
+  
+      res.json({ message: 'Password changed successfully' });
+  
+    } catch (error) {
+  
+      console.error(error);
+      res.status(400).json({ error: error.message || 'Failed to change password' });
+      
+    }
+}
