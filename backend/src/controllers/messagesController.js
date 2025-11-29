fe9bff223a606394250809cc66c4b4bfc1236065
Anish Kumar <anishkkrishkumar@gmail.com>
2025-11-28 18:26:50 -0800

    scaffolded logic for message implementation

---
+++ backend/src/controllers/messagesController.js
@@ -0,0 +1,25 @@
+import { supabase } from '../supabase.js';
+import { createMessage, getMessagesForRide } from '../services/messageService.js';
+
+
+// POST /api/messages
+export async function postMessage(req, res, next) {
+    try {
+        // todo
+
+    } catch (error) {
+        //temp
+    }
+}
+
+// GET /api/messages
+export async function getMessages(req, res, next) {
+    try{
+        //todo
+
+
+    } catch (error) {
+        //temp
+    }
+}
+
---
+++ backend/src/services/messageService.js
@@ -0,0 +1,9 @@
+import { supabase } from '../supabase.js';
+
+export async function createMessage(rideId, userId) {
+
+}
+
+export async function getMessagesForRide(rideId, userId) {
+    
+}
