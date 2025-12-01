abff38e212baa2f8ac7bd1e5f38eb60b56d324de
Anish Kumar <anishkkrishkumar@gmail.com>
2025-11-30 22:07:06 -0800

    added proper imports to MyRides.jsx

--- frontend/bruinsplit/src/pages/MyRides.jsx
+++ frontend/bruinsplit/src/pages/MyRides.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './pages.css';
+import { getMyRides, deleteRide, leaveRide } from './api/rides.js';
+
 
 export default function MyRides() {
-    return <h1>My Rides</h1>;
+    
+
+    
+
+
 }
