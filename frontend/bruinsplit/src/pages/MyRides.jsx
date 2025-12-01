448fd013d79ba8a12cc3868bfc1d212305fc21f4
Anish Kumar <anishkkrishkumar@gmail.com>
2025-12-01 11:50:47 -0800

    wrote loadMyRides() for myRides FE page -> calls backend API

--- frontend/bruinsplit/src/pages/MyRides.jsx
+++ frontend/bruinsplit/src/pages/MyRides.jsx
@@ -5,8 +5,57 @@ import { getMyRides, deleteRide, leaveRide } from './api/rides.js';
 
 export default function MyRides() {
     
+    // set variables to hold data and UI state
+     const [ownedRides, setOwnedRides] = useState([]);
+     const [joinedRides, setJoinedRides] = useState([]);
+     const [loading, setLoading] = useState(true);
+     const [error, setError] = useState(null);
 
+     useEffect( () => {
+        loadMyRides();
+     }, []);
+
+     // call backend API
+     async function loadMyRides() {
+        setLoading(true);
+        setError(null);
+
+        try {
+
+            // call API function from api/rides.js
+            const response = await getMyRides();
     
+            const allRides = response.rides || [];
+    
+            // filter based on user_role field from backend
+    
+            const owned = allRides.filter(ride =>
+                ride.user_role === 'owner' || ride.user_role === 'owner_and_member'
+            );
+    
+            const joined = allRides.filter(ride =>
+                ride.user_role === 'member'
+            );
+    
+            // update state with separated rides
+            setOwnedRides(owned);
+            setJoinedRides(joined);
+    
+        } catch (err) {
+            console.error('Error loading rieds:', err);
+            setError(err.response?.data?.error || err.message || 'Failed to load rides');
+        } finally {
+            setLoading(false);
+        }
+
+     }
+
+     async function handleDeleteRide(rideId) {
+        // confirm before deleting ride
+        if (!window.confirm('Are you sure you want to delete this ride?')) {
+            return; // user cancelled ride deletion
+        }
+     }
 
 
 }
