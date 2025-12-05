565d9069365de74b7f582124a02117f7489ba474
jadenho10 <jaden.ho@gmail.com>
2025-12-05 12:29:43 -0800

    Added bear image to login

--- frontend/bruinsplit/src/pages/Login.jsx
+++ frontend/bruinsplit/src/pages/Login.jsx
@@ -4,7 +4,10 @@ import LoginSignup from '../components/loginsignup';
 
 export default function Login() {
   return (<>
-        <div className="page-container">
+        <div className="page-container login-page-right">
+            <div className='login-left-image'>
+                <img src="/download.png" alt="BruinSplit" />
+            </div>
             <div className='Login-Box'>
                 <LoginSignup />
             </div>
