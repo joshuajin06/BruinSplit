f66a3c2e05e221b752a3cab85170588ac08e453e
jadenho10 <jaden.ho@gmail.com>
2025-12-05 19:16:49 -0800

    Refactoring

--- frontend/bruinsplit/src/components/e2e_tests/navbar.test.jsx
+++ frontend/bruinsplit/src/components/e2e_tests/navbar.test.jsx
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import Navbar from '../navbar';
 import * as AuthContext from '../../context/AuthContext';
 
-// Mock dependencies
 jest.mock('../../context/AuthContext');
 jest.mock('../../assets/finalogofinal.png', () => 'mocked-logo.png');
 
